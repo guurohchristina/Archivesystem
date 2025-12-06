9306a7f3c4fbd210ea095337006eb76a4c138d23
guurohchristina <guurohchristina@gmail.com>
2025-12-06 18:28:54 +0000

    update

--- achievebackend/src/server.js
+++ achievebackend/src/server.js
@@ -529,7 +529,7 @@ app.use((req, res, next) => {
 // =========== MIDDLEWARE ===========
 // CORS
 app.use(cors({
-  origin: ["http://localhost:5173", ],
+  origin: ['http://localhost:3000', 'http://localhost:5173'],
   credentials: true
 }));
 
