c0c84c9e6f661379e98d9212b294ed592dac21d7
guurohchristina <guurohchristina@gmail.com>
2025-12-09 05:35:56 +0000

    update Dashboard.jsx

--- achievesystem/src/pages/Dashboard.jsx
+++ achievesystem/src/pages/Dashboard.jsx
@@ -97,7 +97,7 @@ const Dashboard = () => {
   ];
   
   
-  
+  /*
  const getCategoryItems = () => {
     return [
       { 
@@ -188,7 +188,7 @@ const Dashboard = () => {
     return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
   };
 
-  
+  */
   
   
   
