3be3158237eac02173068352b87e0050a001466f
guurohchristina <guurohchristina@gmail.com>
2025-12-09 21:35:29 +0000

    update App.jsx

--- achievesystem/src/App.jsx
+++ achievesystem/src/App.jsx
@@ -35,6 +35,10 @@ function App() {
           <Route path="/register" element={<Register />} />
 
           <Route path="/dashboard" element={<Dashboard />} />
+          
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          
+          
           <Route path="/profile" element={<Profile />} />
           <Route path="/upload" element={<Upload />} />
 
