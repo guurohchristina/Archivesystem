ce261e88d884cdd0ed92c887a76c572cc6efba7f
guurohchristina <guurohchristina@gmail.com>
2025-12-14 05:33:36 +0000

    update vite.config.js

--- achievesystem/vite.config.js
+++ achievesystem/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+/*export default defineConfig({
   plugins: [
     react({
       babel: {
@@ -10,4 +10,18 @@ export default defineConfig({
       },
     }),
   ],
+})*/
+
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/',  // ← ADD THIS LINE FOR RENDER
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]'
+      }
+    }
+  }
 })
