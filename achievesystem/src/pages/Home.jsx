3626b6e472b2281f7dfa72b1fb2d08f7e1e45316
guurohchristina <guurohchristina@gmail.com>
2025-12-15 04:17:30 +0000

    update Home.jsx

--- achievesystem/src/pages/Home.jsx
+++ achievesystem/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import Navbar from "./components/Navbar.jsx";
+import Navbar from "../components/Navbar.jsx";
 
 const Home = () => {
   const [expandedService, setExpandedService] = useState(null);
