d5e06183a83f50fe9e41f0a2a5b08a9d2c05c264
guurohchristina <guurohchristina@gmail.com>
2025-12-15 04:47:20 +0000

    update Home.jsx

--- achievesystem/src/pages/Home.jsx
+++ achievesystem/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthProvider } from "../context/AuthContext.jsx";
 import Navbar from "../components/Navbar.jsx";
 
 const Home = () => {
