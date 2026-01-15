ceccb823fac265886529cb2ad2b824c26f1a3a5b
guurohchristina <guurohchristina@gmail.com>
2026-01-15 01:21:33 +0000

    update folderControllers.js

--- achievebackend/src/controllers/folderControllers.js
+++ achievebackend/src/controllers/folderControllers.js
@@ -667,7 +667,7 @@ export const getBreadcrumbs = async (req, res) => {
 
 
 // Rename folder
- const renameFolder = async (req, res) => {
+export const renameFolder = async (req, res) => {
   try {
     const { id } = req.params;
     const { name } = req.body;
